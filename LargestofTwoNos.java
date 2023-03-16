a86532d14562fa7330cf8ffe2ffdb7d2ad81f1e3
Gokul Govinda <87459046+gokulgovinda@users.noreply.github.com>
2023-03-16 21:58:17 +0530

    Create LargestofTwoNos.java

---
+++ LargestofTwoNos.java
@@ -0,0 +1,15 @@
+class LargestofTwoNos {
+    public static void main(String[] args) {
+      
+        int a=1;
+        int b=3;
+        if(a>=b){
+            System.out.println("a is largest of 2 numbers");
+
+        }
+        else{
+            System.out.println("b is largest of 2 numbers");
+        }
+        
+    }
+}
